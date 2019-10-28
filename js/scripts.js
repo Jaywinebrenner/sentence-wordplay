7d0a8c4d2b4d238f1f5528de2d2bac028938d39c
Student <example@example.com>
2019-10-28 16:47:43 -0700

    add intial files, add first array

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  $(".form").submit(function(event){
+    event.preventDefault();
+
+    var userInput = $("#inputSentence").val();
+
+    var sentenceArray = userInput.split(" ")
+//made new array!!!!!!
+
+
+
+  })
+})
